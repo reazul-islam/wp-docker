a1378e5c2dc8dd9877872a01b4d9aa7eeef09359
Reazul Islam <reazul@ackosoft.com>
2019-11-07 01:08:28 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Stop running containers and remove related directories
-read -p "Do you really want to install Wordpress-Nginx-Mariadb (y/n)? " answer
+read -p "Do you really want to install [DEV] Wordpress-Nginx-Mariadb (y/n)? " answer
 case ${answer:0:1} in
     y|Y )
         echo "INFO: Installation started, please be patient."
         docker-compose up -d
 	sleep 5s
-	docker exec --user root -it wordpress apt-get update -y
-	docker exec --user root -it wordpress apt-get install nano -y
+	docker exec --user root -it wordpress_dev apt-get update -y
+	docker exec --user root -it wordpress_dev apt-get install nano -y
         echo "INFO: Installation completed"
         exit 0;
     ;;
